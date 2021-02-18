166bf6b640628d575c904a1a0c6c03a94aff277c
Júnior Conqusta <juniorconqusta@MacBook-Pro-de-Fabricio.local>
2021-02-17 23:50:46 -0300

    init serverless start

---
+++ serverless.ts
@@ -0,0 +1,31 @@
+import type { AWS } from '@serverless/typescript';
+
+import { getProducts } from './src/functions';
+
+const serverlessConfiguration: AWS = {
+  service: 'urban-store',
+  frameworkVersion: '2',
+  custom: {
+    webpack: {
+      webpackConfig: './webpack.config.js',
+      includeModules: true
+    }
+  },
+  plugins: ['serverless-webpack'],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs12.x',
+    region: 'us-west-2',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+    },
+    lambdaHashingVersion: '20201221',
+  },
+  functions: { getProducts }
+}
+
+module.exports = serverlessConfiguration;
